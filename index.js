a0ba52fe3fd47bef705d0906a3835d6d86e3b1fc
Jared Collier <jared@Jareds-MBP.localdomain>
2022-05-25 12:23:51 -0600

    fixed tie breaker issue

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let jamieLannisterAttack = 35;
 
 if (jonSnowAttack > jamieLannisterAttack) {
     console.log("Jon Snow has better attack than Jamie Lannister");
-} else {
+} else if (jonSnowAttack < jamieLannisterAttack) {
     console.log("Jamie Lannister has better attack than Jon Snow");
+} else {
+    console.log("Jon Snow and Jamie Lanniister have the same attack");
 }
